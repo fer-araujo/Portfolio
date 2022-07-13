4b91e5acd549c95186a67a380b2b7526daff7bab
Fer Araujo <fer_araujo@live.com.mx>
2022-07-13 16:27:50 -0500

    change color and responsiveness

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
-
+console.log("check token", process.env.REACT_APP_SANITY_PROJECT_ID)
 export const client = sanityClient({
     projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
